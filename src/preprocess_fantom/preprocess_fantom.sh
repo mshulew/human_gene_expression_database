6801c8a92fa68d0c355741394036ce067f69f4c3
Mark Shulewitz <markshulewitz@gmail.com>
2023-12-31 18:44:42 -0800

    annotate bash scripts for preprocessing FANTOM data

--- src/preprocess_fantom/preprocess_fantom.sh
+++ src/preprocess_fantom/preprocess_fantom.sh
@@ -4,7 +4,7 @@ set -e
 # preprocess FANTOM data to import into pandas
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# make list of samples
+# make list of samples and corresponding column number
 if [[ $1 == *"tpm"* ]]; then
 	bash ${SCRIPT_DIR}/getSampleListTPM.sh $1 > sampleList0001
 
@@ -15,11 +15,11 @@ else
 	echo "check input file"
 fi
 
-# create list of columns to filter
+# create string of column numbers to filter
 columnsToFilter=$(bash ${SCRIPT_DIR}/getCols.sh sampleList0001)
 rm sampleList0001
 
-# filter columns
+# filter columns by number
 bash ${SCRIPT_DIR}/filterByCols.sh $1 ${columnsToFilter} > filteredDataSet0002
 
 # remove rows with no GeneID
